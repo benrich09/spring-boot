9404b488343ffa764a64f0721e32e53e6fd64b9a
BEN RICH <benrich205@gmail.com>
2025-08-22 12:43:34 +0300

    second commit

---
+++ src/main/java/com/example/springboot/SoftwareEngineerController.java
@@ -0,0 +1,29 @@
+package com.example.springboot;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers() {
+        return List.of(
+                new SoftwareEngineer(
+                        1,
+                        "James",
+                        "js, node, react, tailwindcss"
+                ),
+                new SoftwareEngineer(
+                        2,
+                        "Jamila",
+                        "java, springboot, spring"
+                )
+
+        );
+    }
+}
